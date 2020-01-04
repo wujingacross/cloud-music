f0db381b382ba03615d08f83923bdbfdd1dd743d
wujing <wujing@1d1d100.com>
2020-01-04 22:18:57 +0800

    refactor: 图片懒加载、loading、Redux 数据缓存

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,19 +1,27 @@
 import React, { memo, useEffect } from 'react';
 import { connect } from "react-redux";
+import { forceCheck } from 'react-lazyload';
 import * as actionTypes from './store/actionCreators';
 import Slider from '../../components/slider';
+import Loading from '../../baseUI/loading/index';
 import RecommendList from '../../components/list';
 import Scroll from '../../baseUI/scroll';
 import { Content } from './style';
 
 function Recommend (props) {
-    const { bannerList, recommendList } = props;
+    const { bannerList, recommendList, enterLoading } = props;
     const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
 
     useEffect (() => {
+      // 如果页面有数据，则不发请求
+      //immutable 数据结构中长度属性 size
+      if (!bannerList.size){
         getBannerDataDispatch ();
+      }
+      if (!recommendList.size){
         getRecommendListDataDispatch ();
-        //eslint-disable-next-line
+      }
+      //eslint-disable-next-line
     }, []);
 
     const bannerListJS = bannerList ? bannerList.toJS () : [];
@@ -35,12 +43,13 @@ function Recommend (props) {
 
     return (
         <Content>
-            <Scroll>
-                <div>
-                    <Slider bannerList={bannerListJS}></Slider>
-                    <RecommendList recommendList={recommendListJS}></RecommendList>
-                </div>
-            </Scroll>
+          <Scroll onScroll={forceCheck}>
+              <div>
+                  <Slider bannerList={bannerListJS}></Slider>
+                  <RecommendList recommendList={recommendListJS}></RecommendList>
+              </div>
+          </Scroll>
+          { enterLoading ? <Loading></Loading> : null }
         </Content> 
     )
 }
@@ -51,6 +60,7 @@ const mapStateToProps = (state) => ({
     // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
     bannerList: state.getIn (['recommend', 'bannerList']),
     recommendList: state.getIn (['recommend', 'recommendList']),
+    enterLoading: state.getIn (['recommend', 'enterLoading'])
   });
   // 映射 dispatch 到 props 上
   const mapDispatchToProps = (dispatch) => {
